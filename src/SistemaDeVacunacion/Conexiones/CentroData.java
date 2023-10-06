33bea8303a049de8849b6191a907394e168a93d9
WalterDjb <walterdjb@gmail.com>
2023-10-06 18:32:26 -0300

    Actualizacion CentroData

--- src/SistemaDeVacunacion/Conexiones/CentroData.java
+++ src/SistemaDeVacunacion/Conexiones/CentroData.java
@@ -2,10 +2,8 @@ package SistemaDeVacunacion.Conexiones;
 
 import SistemaDeVacunacion.Entidades.Centro;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.sql.Statement;
 import javax.swing.JOptionPane;
 
 public class CentroData {
