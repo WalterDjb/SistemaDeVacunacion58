1187f73a073d8a7186272d2dda4a2c132f0abf44
GuilleDawson <GUILLEGLP@HOTMAIL.COM>
2023-10-16 13:22:20 -0300

    la de informacion

--- src/SistemaDeVacunacion/Vistas/Informacion.java
+++ src/SistemaDeVacunacion/Vistas/Informacion.java
@@ -27,13 +27,115 @@ public class Informacion extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
+        jLabel7 = new javax.swing.JLabel();
+        jLabel8 = new javax.swing.JLabel();
+        jLabel9 = new javax.swing.JLabel();
+        jLabel10 = new javax.swing.JLabel();
+        jLabel13 = new javax.swing.JLabel();
+        jLabel14 = new javax.swing.JLabel();
+        jLabel15 = new javax.swing.JLabel();
+        jLabel16 = new javax.swing.JLabel();
+        jLabel17 = new javax.swing.JLabel();
         jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setIconImage(getIconImage());
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/SistemaDeVacunacion/Vistas/Fondo app.png"))); // NOI18N
+        jLabel3.setFont(new java.awt.Font("Serif", 1, 30)); // NOI18N
+        jLabel3.setForeground(new java.awt.Color(15, 75, 94));
+        jLabel3.setText("SINTOMAS");
+        getContentPane().add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(300, 310, 230, 40));
+
+        jLabel4.setFont(new java.awt.Font("Serif", 1, 30)); // NOI18N
+        jLabel4.setForeground(new java.awt.Color(15, 75, 94));
+        jLabel4.setText("CUIDADOS");
+        getContentPane().add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(300, 110, 230, 40));
+
+        jLabel2.setFont(new java.awt.Font("Verdana", 1, 13)); // NOI18N
+        jLabel2.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
+        jLabel2.setText("<html><body style='width: 140px; text-align: justify;'>No llevarse las manos a los ojos, nariz ni boca</body></html>");
+        jLabel2.setVerticalAlignment(javax.swing.SwingConstants.TOP);
+        jLabel2.setVerticalTextPosition(javax.swing.SwingConstants.TOP);
+        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(610, 230, 190, 50));
+
+        jLabel6.setFont(new java.awt.Font("Verdana", 1, 13)); // NOI18N
+        jLabel6.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
+        jLabel6.setText("<html><body style='width: 140px; text-align: justify;'>Estornudar en el pliegue del codo</body></html>");
+        jLabel6.setVerticalAlignment(javax.swing.SwingConstants.TOP);
+        jLabel6.setVerticalTextPosition(javax.swing.SwingConstants.TOP);
+        getContentPane().add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 230, 190, 50));
+
+        jLabel7.setFont(new java.awt.Font("Verdana", 1, 13)); // NOI18N
+        jLabel7.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
+        jLabel7.setText("<html><body style='width: 140px; text-align: justify;'>Ventilar los ambientes</body></html>");
+        jLabel7.setVerticalAlignment(javax.swing.SwingConstants.TOP);
+        jLabel7.setVerticalTextPosition(javax.swing.SwingConstants.TOP);
+        getContentPane().add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(350, 170, 190, 50));
+
+        jLabel8.setFont(new java.awt.Font("Verdana", 1, 13)); // NOI18N
+        jLabel8.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
+        jLabel8.setText("<html><body style='width: 140px; text-align: justify;'>Limpiar los objetos que se usan con frecuencia</body></html>");
+        jLabel8.setVerticalAlignment(javax.swing.SwingConstants.TOP);
+        jLabel8.setVerticalTextPosition(javax.swing.SwingConstants.TOP);
+        getContentPane().add(jLabel8, new org.netbeans.lib.awtextra.AbsoluteConstraints(350, 230, 190, 50));
+
+        jLabel9.setFont(new java.awt.Font("Verdana", 1, 13)); // NOI18N
+        jLabel9.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
+        jLabel9.setText("<html><body style='width: 130px; text-align: justify;'>No compartir platos, vasos u otros artìculos de uso personal</body></html>");
+        jLabel9.setVerticalAlignment(javax.swing.SwingConstants.TOP);
+        jLabel9.setVerticalTextPosition(javax.swing.SwingConstants.TOP);
+        getContentPane().add(jLabel9, new org.netbeans.lib.awtextra.AbsoluteConstraints(610, 160, 190, 50));
+
+        jLabel10.setFont(new java.awt.Font("Verdana", 1, 13)); // NOI18N
+        jLabel10.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
+        jLabel10.setText("<html><body style='width: 135px; text-align: justify;'>Lavarse las manos con jabòn regularmente</body></html>");
+        jLabel10.setVerticalAlignment(javax.swing.SwingConstants.TOP);
+        jLabel10.setVerticalTextPosition(javax.swing.SwingConstants.TOP);
+        getContentPane().add(jLabel10, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 170, 190, 50));
+
+        jLabel13.setFont(new java.awt.Font("Verdana", 1, 18)); // NOI18N
+        jLabel13.setForeground(new java.awt.Color(15, 75, 94));
+        jLabel13.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
+        jLabel13.setText("<html><body style='width: 600px; text-align: justify;'>NO TE AUTOMEDIQUES y comunicate al 0800 222 0000</body></html>");
+        jLabel13.setVerticalAlignment(javax.swing.SwingConstants.TOP);
+        jLabel13.setVerticalTextPosition(javax.swing.SwingConstants.TOP);
+        getContentPane().add(jLabel13, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 480, 580, 30));
+
+        jLabel14.setFont(new java.awt.Font("Verdana", 0, 16)); // NOI18N
+        jLabel14.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
+        jLabel14.setText("<html><body style='width: 5px; text-align: justify;'>Fiebre y tos</body></html>");
+        jLabel14.setVerticalAlignment(javax.swing.SwingConstants.TOP);
+        jLabel14.setVerticalTextPosition(javax.swing.SwingConstants.TOP);
+        getContentPane().add(jLabel14, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 355, 150, 80));
+
+        jLabel15.setFont(new java.awt.Font("Verdana", 0, 16)); // NOI18N
+        jLabel15.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
+        jLabel15.setText("<html><body style='width: 50px; text-align: justify;'>Fiebre y dolor de garganta</body></html>");
+        jLabel15.setVerticalAlignment(javax.swing.SwingConstants.TOP);
+        jLabel15.setVerticalTextPosition(javax.swing.SwingConstants.TOP);
+        getContentPane().add(jLabel15, new org.netbeans.lib.awtextra.AbsoluteConstraints(410, 355, 150, 110));
+
+        jLabel16.setFont(new java.awt.Font("Verdana", 0, 16)); // NOI18N
+        jLabel16.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
+        jLabel16.setText("<html><body style='width: 65px; text-align: justify;'>Fiebre y dificultad para respirar</body></html>");
+        jLabel16.setVerticalAlignment(javax.swing.SwingConstants.TOP);
+        jLabel16.setVerticalTextPosition(javax.swing.SwingConstants.TOP);
+        getContentPane().add(jLabel16, new org.netbeans.lib.awtextra.AbsoluteConstraints(660, 355, 150, 110));
+
+        jLabel17.setFont(new java.awt.Font("Verdana", 0, 16)); // NOI18N
+        jLabel17.setForeground(new java.awt.Color(15, 75, 94));
+        jLabel17.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
+        jLabel17.setText("<html><body style='width: 600px; text-align: justify;'>Si tenes alguno de estos sintomas o en contacto con algùn caso confirmado</body></html>");
+        jLabel17.setVerticalAlignment(javax.swing.SwingConstants.TOP);
+        jLabel17.setVerticalTextPosition(javax.swing.SwingConstants.TOP);
+        getContentPane().add(jLabel17, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 460, 700, 20));
+
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/SistemaDeVacunacion/Vistas/informacion_3.png"))); // NOI18N
         getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, 600));
 
         pack();
@@ -70,6 +172,19 @@ public class Informacion extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel10;
+    private javax.swing.JLabel jLabel13;
+    private javax.swing.JLabel jLabel14;
+    private javax.swing.JLabel jLabel15;
+    private javax.swing.JLabel jLabel16;
+    private javax.swing.JLabel jLabel17;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
+    private javax.swing.JLabel jLabel8;
+    private javax.swing.JLabel jLabel9;
     // End of variables declaration//GEN-END:variables
 
 }
