32056404d09918a1e21fe56f55e6ccc76425a1a2
GuilleDawson <GUILLEGLP@HOTMAIL.COM>
2023-10-29 12:10:41 -0300

    .

--- src/SistemaDeVacunacion/Vistas/CentrosHabilitados.java
+++ src/SistemaDeVacunacion/Vistas/CentrosHabilitados.java
@@ -37,6 +37,7 @@ public class CentrosHabilitados extends javax.swing.JFrame {
         jLabel3 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
         JCprovincia = new javax.swing.JComboBox<>();
+        jLabel5 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -44,11 +45,11 @@ public class CentrosHabilitados extends javax.swing.JFrame {
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         JCcentros.setFont(new java.awt.Font("Verdana", 0, 16)); // NOI18N
-        getContentPane().add(JCcentros, new org.netbeans.lib.awtextra.AbsoluteConstraints(445, 270, 190, 40));
+        getContentPane().add(JCcentros, new org.netbeans.lib.awtextra.AbsoluteConstraints(445, 270, 290, 40));
 
         jLabel1.setFont(new java.awt.Font("Verdana", 0, 16)); // NOI18N
-        jLabel1.setText("Seleccione el centro de salud deseado");
-        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 280, -1, -1));
+        jLabel1.setText("Seleccione el centro de salud");
+        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 280, -1, -1));
 
         jbAcceder.setBackground(new java.awt.Color(15, 75, 94));
         jbAcceder.setFont(new java.awt.Font("Verdana", 0, 16)); // NOI18N
@@ -74,11 +75,12 @@ public class CentrosHabilitados extends javax.swing.JFrame {
 
         jLabel3.setFont(new java.awt.Font("Serif", 1, 30)); // NOI18N
         jLabel3.setForeground(new java.awt.Color(15, 75, 94));
-        jLabel3.setText("CENTROS HABILITADOS");
-        getContentPane().add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(220, 150, -1, -1));
+        jLabel3.setText("Consulte las direcciones de los");
+        getContentPane().add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(210, 110, -1, -1));
 
+        jLabel4.setFont(new java.awt.Font("Verdana", 0, 16)); // NOI18N
         jLabel4.setText("Seleccione una Provincia");
-        getContentPane().add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(200, 210, -1, -1));
+        getContentPane().add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(210, 210, -1, -1));
 
         JCprovincia.setFont(new java.awt.Font("Verdana", 0, 16)); // NOI18N
         JCprovincia.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Buenos Aires", "Ciudad Autónoma de Buenos Aires", "Catamarca", "Chaco", "Chubut", "Córdoba", "Corrientes", "Entre Ríos", "Formosa", "Jujuy", "La Pampa", "La Rioja", "Mendoza", "Misiones", "Neuquén", "Río Negro", "Salta", "San Juan", "San Luis", "Santa Cruz", "Santa Fe", "Santiago del Estero", "Tierra del Fuego", "Tucumán" }));
@@ -87,7 +89,12 @@ public class CentrosHabilitados extends javax.swing.JFrame {
                 JCprovinciaActionPerformed(evt);
             }
         });
-        getContentPane().add(JCprovincia, new org.netbeans.lib.awtextra.AbsoluteConstraints(420, 200, -1, 40));
+        getContentPane().add(JCprovincia, new org.netbeans.lib.awtextra.AbsoluteConstraints(446, 200, 290, 40));
+
+        jLabel5.setFont(new java.awt.Font("Serif", 1, 30)); // NOI18N
+        jLabel5.setForeground(new java.awt.Color(15, 75, 94));
+        jLabel5.setText("CENTROS HABILITADOS");
+        getContentPane().add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(220, 150, -1, -1));
 
         jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/SistemaDeVacunacion/Vistas/Fondo app.png"))); // NOI18N
         getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 800, 600));
@@ -148,6 +155,7 @@ public class CentrosHabilitados extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JButton jbAcceder;
     private javax.swing.JButton jbVolver;
