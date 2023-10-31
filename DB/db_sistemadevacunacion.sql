ff0a80831e4cc8589b80d38686b09281578db303
GstrXXX <101074574+GstrXXX@users.noreply.github.com>
2023-10-31 17:46:24 -0300

    Update db_sistemadevacunacion.sql

--- DB/db_sistemadevacunacion.sql
+++ DB/db_sistemadevacunacion.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 31-10-2023 a las 04:05:40
+-- Tiempo de generación: 31-10-2023 a las 21:45:17
 -- Versión del servidor: 10.4.22-MariaDB
 -- Versión de PHP: 8.1.1
 
@@ -58,6 +58,42 @@ INSERT INTO `acceso` (`nivel`, `nombre`, `email`, `usuario`, `contra`, `tipoCont
 (3, 'Centro 48', '-', '48-Barracas', '48-Barracas', 'A'),
 (3, 'Centro 49', '-', '49-Santa Lucía', '49-Santa Lucía', 'A'),
 (3, 'Centro 50', '-', '50-Santo Tomé', '50-Santo Tomé', 'A'),
+(3, 'Centro 51', '-', '51-Capital', '51-Capital', 'A'),
+(3, 'Centro 52', '-', '52-Chilecito', '52-Chilecito', 'A'),
+(3, 'Centro 53', '-', '53-Capital', '53-Capital', 'A'),
+(3, 'Centro 54', '-', '54-CataNoMarca', '54-CataNoMarca', 'A'),
+(3, 'Centro 55', '-', '55-Capital', '55-Capital', 'A'),
+(3, 'Centro 56', '-', '56-Saenz Peña', '56-Saenz Peña', 'A'),
+(3, 'Centro 57', '-', '57-Capital', '57-Capital', 'A'),
+(3, 'Centro 58', '-', '58-Pueblito', '58-Pueblito', 'A'),
+(3, 'Centro 59', '-', '59-Capital', '59-Capital', 'A'),
+(3, 'Centro 60', '-', '60-VillaCarlosPaz', '60-VillaCarlosPaz', 'A'),
+(3, 'Centro 61', '-', '61-Capital', '61-Capital', 'A'),
+(3, 'Centro 62', '-', '62-La Paz', '62-La Paz', 'A'),
+(3, 'Centro 63', '-', '63-Capital', '63-Capital', 'A'),
+(3, 'Centro 64', '-', '64-La Primavera', '64-La Primavera', 'A'),
+(3, 'Centro 65', '-', '65-Capital', '65-Capital', 'A'),
+(3, 'Centro 66', '-', '66-Tunuyan', '66-Tunuyan', 'A'),
+(3, 'Centro 67', '-', '67-Capital', '67-Capital', 'A'),
+(3, 'Centro 68', '-', '68-Nowhere', '68-Nowhere', 'A'),
+(3, 'Centro 69', '-', '69-Capital', '69-Capital', 'A'),
+(3, 'Centro 70', '-', '70-La Punta', '70-La Punta', 'A'),
+(3, 'Centro 71', '-', '71-Capital', '71-Capital', 'A'),
+(3, 'Centro 72', '-', '72-Puerto Olvidado', '72-Puerto Olvidado', 'A'),
+(3, 'Centro 73', '-', '73-Capital', '73-Capital', 'A'),
+(3, 'Centro 74', '-', '74-PuebloSolYTierra', '74-PuebloSolYTierra', 'A'),
+(3, 'Centro 75', '-', '75-Capital', '75-Capital', 'A'),
+(3, 'Centro 76', '-', '76-Rosario', '76-Rosario', 'A'),
+(3, 'Centro 77', '-', '77-Capital', '77-Capital', 'A'),
+(3, 'Centro 78', '-', '78-PueblitoInospito', '78-PueblitoInospito', 'A'),
+(3, 'Centro 79', '-', '79-Capital', '79-Capital', 'A'),
+(3, 'Centro 80', '-', '80-Rio Blanco', '80-Rio Blanco', 'A'),
+(3, 'Centro 81', '-', '81-Capital', '81-Capital', 'A'),
+(3, 'Centro 82', '-', '82-Piedritas', '82-Piedritas', 'A'),
+(3, 'Centro 83', '-', '83-Capital', '83-Capital', 'A'),
+(3, 'Centro 84', '-', '84-SeSienta', '84-SeSienta', 'A'),
+(3, 'Centro 85', '-', '85-Capital', '85-Capital', 'A'),
+(3, 'Centro 86', '-', '86-Tolhuin', '86-Tolhuin', 'A'),
 (2, 'Guille', 'guilleglp@gmail.com', 'Buenos Aires', 'BuenosAires', 'A'),
 (2, '-', '-', 'CABA', 'CABA', 'A'),
 (2, '-', '-', 'Catamarca', 'Catamarca', 'A'),
@@ -111,7 +147,7 @@ INSERT INTO `centro` (`id`, `direccion`, `stock`, `provincia`, `localidad`, `cap
 (39, 'Beron de Astrada 20', NULL, 'Corrientes', 'Curuzu Cuatia', 50, 0),
 (40, 'Paraguay 1653', NULL, 'Corrientes', 'Ituzaingo', 100, 0),
 (41, 'Menem1989', NULL, 'Corrientes', 'La Cruz', 60, 0),
-(42, '3 de abril 1968', NULL, 'Corrientes', 'Capital2', 150, 0),
+(42, '3 de abril 1968', NULL, 'Corrientes', 'Capital', 150, 0),
 (43, 'Caa Guazu 1800', NULL, 'Corrientes', 'Mercedes', 75, 0),
 (44, 'Independencia 660', NULL, 'Corrientes', 'Saladas', 30, 0),
 (45, 'San Martin 5960', NULL, 'Buenos Aires', 'La Plata', 250, 0),
@@ -119,7 +155,43 @@ INSERT INTO `centro` (`id`, `direccion`, `stock`, `provincia`, `localidad`, `cap
 (47, 'San Juan 1490', NULL, 'Ciudad Autónoma de Buenos Aires', 'San Telmo', 250, 0),
 (48, 'Defensa 2550', NULL, 'Ciudad Autónoma de Buenos Aires', 'Barracas', 200, 0),
 (49, 'Belgrano 1280', NULL, 'Corrientes', 'Santa Lucía', 50, 0),
-(50, 'vaca marron 111', NULL, 'Corrientes', 'Santo Tomé', 10, 0);
+(50, 'vaca marron 111', NULL, 'Corrientes', 'Santo Tomé', 10, 0),
+(51, 'Carlos Saul 333', NULL, 'La Rioja', 'Capital', 100, 0),
+(52, 'España 959', NULL, 'La Rioja', 'Chilecito', 50, 0),
+(53, 'Mitre 653', NULL, 'Catamarca', 'Capital', 150, 0),
+(54, 'DireccionDeMentira 33', NULL, 'Catamarca', 'CataNoMarca', 80, 0),
+(55, 'Barrio Toba C55', NULL, 'Chaco', 'Capital', 10, 0),
+(56, 'José Hernandez 564', NULL, 'Chaco', 'Saenz Peña', 200, 0),
+(57, 'Lucas Matthysse 1', NULL, 'Chubut', 'Capital', 250, 0),
+(58, 'Direccion 666', NULL, 'Chubut', 'Pueblito', 75, 0),
+(59, 'Fernet Branca 369', NULL, 'Cordoba', 'Capital', 200, 0),
+(60, 'Vittone 147', NULL, 'Cordoba', 'VillaCarlosPaz', 10, 0),
+(61, 'Coso 987', NULL, 'Entre Rios', 'Capital', 200, 0),
+(62, 'Estem 520', NULL, 'Entre Rios', 'La Paz', 100, 0),
+(63, 'Tio Gildo 666', NULL, 'Formosa', 'Capital', 300, 0),
+(64, 'Tio Gildo 333', NULL, 'Formosa', 'La Primavera', 100, 0),
+(65, 'Malvec 123', NULL, 'Mendoza', 'Capital', 250, 0),
+(66, 'Sidra 555', NULL, 'Mendoza', 'Tunuyan', 100, 0),
+(67, 'TierraYPiedras 456', NULL, 'Neuquen', 'Capital', 250, 0),
+(68, '159', NULL, 'Neuquen', 'Nowhere', 50, 0),
+(69, 'Homero Tomadin 1', NULL, 'San Luis', 'Capital', 250, 0),
+(70, 'Carlos Pallotti 001', NULL, 'San Luis', 'La Punta', 300, 0),
+(71, 'Tio Nestor 666', NULL, 'Santa Cruz', 'Capital', 10, 0),
+(72, 'Tio Nestor 666', NULL, 'Santa Cruz', 'Puerto Olvidado', 10, 0),
+(73, 'David Moya 333', NULL, 'Tucuman', 'Capital', 250, 0),
+(74, 'Juana Zurduy 123', NULL, 'Tucuman', 'PuebloSolYTierra', 100, 0),
+(75, 'Tio Binner 666', NULL, 'Santa Fe', 'Capital', 250, 0),
+(76, 'Trinchera15 666', NULL, 'Santa Fe', 'Rosario', 150, 0),
+(77, 'Ventolera 753', NULL, 'San Juan', 'Capital', 200, 0),
+(78, 'Calle Unica 951', NULL, 'San Juan', 'PueblitoInospito', 50, 0),
+(79, 'NoSeMeOcurre 654', NULL, 'Rio Negro', 'Capital', 175, 0),
+(80, 'Inventado 885', NULL, 'Rio Negro', 'Rio Blanco', 75, 0),
+(81, 'Llama 25', NULL, 'Jujuy', 'Capital', 300, 0),
+(82, 'Tio Gerardo 666', NULL, 'Jujuy', 'Piedritas', 80, 0),
+(83, 'Una Pata 879', NULL, 'Salta', 'Capital', 350, 0),
+(84, 'Callecita8 552 ', NULL, 'Salta', 'SeSienta', 90, 0),
+(85, 'Onas 553', NULL, 'Tierra del Fuego', 'Capital', 300, 0),
+(86, 'PiedrasYFrio 654', NULL, 'Tierra del Fuego', 'Tolhuin', 80, 0);
 
 -- --------------------------------------------------------
 
@@ -144,9 +216,9 @@ CREATE TABLE `cita` (
 --
 
 INSERT INTO `cita` (`id`, `dni`, `fHCita`, `Vacuna`, `fHAplicacion`, `centro`, `estadoCita`, `numeroSerie`, `Dosis`) VALUES
-(2, 31863250, NULL, NULL, NULL, 39, NULL, NULL, 0),
-(3, 31863250, '2023-10-30 00:00:00', NULL, '2023-10-30 00:00:00', 39, 'CUM', '321', 1),
-(4, 32999440, '2023-10-30 00:00:00', NULL, NULL, 39, 'VEN', NULL, 0);
+(2, 31863250, '2023-11-01 00:00:00', 'vaxina', '2023-10-31 09:15:41', 39, 'CUM', '00010', 10),
+(3, 31863250, '2023-11-01 00:00:00', 'vaxina', '2023-10-31 09:17:09', 39, 'CUM', '1321', 2),
+(4, 32999440, '2023-11-01 00:00:00', 'vaxina', '2023-10-31 09:17:29', 39, 'CUM', '00011', 2);
 
 -- --------------------------------------------------------
 
@@ -175,9 +247,10 @@ CREATE TABLE `ciudadano` (
 --
 
 INSERT INTO `ciudadano` (`dni`, `tramite`, `nombre`, `apellido`, `provincia`, `localidad`, `email`, `celular`, `patologia`, `ambitoTrabajo`, `aplicaciones`, `ultimaDosis`, `domicilio`) VALUES
+(11111111, '22222222222', 'Tiburcio', 'Trafalgar', 'Buenos Aires', 'Tortuguitas', 'tubur@example.com', 321321321321, 'Enf. autoinmunitarias.', 'Educacion', 0, NULL, 'La Casita 123'),
 (11112222, '89012345671', 'Elena', 'García', 'Corrientes', 'Ciudad R', 'elena@example.com', 1818181818, 'Diabetes', 'Educación', 0, NULL, 'Avenida 151, No. 161'),
 (12345649, '12345671890', 'Juan', 'Pérez', 'Buenos Aires', 'Ciudad A', 'juan@example.com', 1111111111, 'Ninguna', 'Oficina', 0, NULL, 'Calle 123, No. 456'),
-(12345678, '12345678910', 'Josecito', 'Lopez', 'Goya', 'Goya', 'sdfadsfasdfads@cacamail.com', 999999999999, 'Cáncer.', 'Justicia', 0, NULL, 'Estado 0001'),
+(12345678, '12345678910', 'Josecito', 'Lopez', 'Corrientes', 'Goya', 'sdfadsfasdfads@cacamail.com', 999999999999, 'Cáncer.', 'Justicia', 0, NULL, 'Estado 0001'),
 (20200202, '01123456789', 'Mariana', 'Díaz', 'Santiago del Estero', 'Ciudad T', 'mariana@example.com', 2020202020, 'Alergia', 'Oficina', 0, NULL, 'Avenida 171, No. 181'),
 (22221110, '78901123456', 'Diego', 'Torres', 'Jujuy', 'Ciudad G', 'diego@example.com', 2147483647, 'Ninguna', 'Salud', 0, NULL, 'Calle 444, No. 555'),
 (23456789, '67891012345', 'María', 'Martínez', 'Tierra del Fuego', 'Ciudad P', 'maria@example.com', 1616161616, 'Ninguna', 'Salud', 0, NULL, 'Avenida 131, No. 141'),
@@ -231,7 +304,7 @@ INSERT INTO `laboratorio` (`cuit`, `nombre`, `pais`, `direccion`, `stock`) VALUE
 CREATE TABLE `stock` (
   `idCentro` int(11) NOT NULL,
   `marca` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
-  `stock` int(11) NOT NULL DEFAULT 0,
+  `stock` double NOT NULL DEFAULT 0,
   `indice` int(50) NOT NULL COMMENT 'Identificador necesario para el servidor, no le den bola'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
@@ -243,7 +316,7 @@ INSERT INTO `stock` (`idCentro`, `marca`, `stock`, `indice`) VALUES
 (36, 'vaxina', 0, 1),
 (37, 'vaxina', 0, 2),
 (38, 'vaxina', 0, 3),
-(39, 'vaxina', 0, 4),
+(39, 'vaxina', 42, 4),
 (40, 'vaxina', 0, 5),
 (41, 'vaxina', 0, 6),
 (42, 'vaxina', 0, 7),
@@ -342,7 +415,7 @@ ALTER TABLE `vacuna`
 -- AUTO_INCREMENT de la tabla `centro`
 --
 ALTER TABLE `centro`
-  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=87;
 
 --
 -- AUTO_INCREMENT de la tabla `cita`
